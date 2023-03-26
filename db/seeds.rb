dff2d73fbf3d28a08b8ff10e1c3f1d7482d0fdcb
Michael Toulouse <wrycoder@ameritech.net>
2023-03-26 11:57:06 +0000

    Adding default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+50.times do
+  Article.create({ title: Cicero.words(5), text: Cicero.words(200) })
+end

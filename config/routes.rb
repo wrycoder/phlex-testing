e6cbc0c14a687bc5c40bc06575556f52e7ed8f0a
Michael Toulouse <wrycoder@ameritech.net>
2023-03-26 11:57:19 +0000

    Changed root to the articles index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :articles, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
